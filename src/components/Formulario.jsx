4373bab68f7a89b9b37fee58bb3e4b1f3e8389fd
Maicoll David <maicoll.ariza.c@gmail.com>
2022-10-18 13:23:25 -0500

    Se agregó estilos al formulario, se instaló firebase con su respectiva configuración.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,43 +7,69 @@ export const Formulario = ({ initialForm }) => {
   return (
     <div>
         <form>
-            <input 
-                type="text"
-                placeholder='Nombre del equipo'
-                name='nombreEquipo'
-                value={ nombreEquipo }
-                onChange={ handleInputChange }
+            <div className='form-floating mb-3'>
+                <input 
+                    className='form-control'
+                    type="text"
+                    placeholder='Nombre del equipo'
+                    id='nombreEquipo'
+                    name='nombreEquipo'
+                    value={ nombreEquipo }
+                    onChange={ handleInputChange }
                 />
-            <input 
-                type="text"
-                placeholder='Nombre del estadio de fútbol'
-                name='nombreEstadio'
-                value={ nombreEstadio }
-                onChange={ handleInputChange }
-                />
-            <input 
-                type="text"
-                placeholder='Nombre del Director técnico'
-                name='nombreTecnico'
-                value={ nombreTecnico }
-                onChange={ handleInputChange }
-                />
-            <input 
-                type="text"
-                placeholder='Nombre del capitán'
-                name='nombreCapitan'
-                value={ nombreCapitan }
-                onChange={ handleInputChange }
-                />
-            <input 
-                type="number"
-                placeholder='Numero de títulos obtenidos'
-                name='canTitulos'
-                value={ canTitulos }
-                onChange={ handleInputChange }
-                />
-            <select name='liga' value={ liga } onInput={ handleInputChange } >
-                <option value="">---Selecciona---</option>
+                <label htmlFor="nombreEquipo">Nombre del equipo</label>
+            </div>
+            <div className='form-floating mb-3'>
+                <input 
+                    className='form-control'
+                    type="text"
+                    placeholder='Nombre del estadio de fútbol'
+                    id="estadio"
+                    name='nombreEstadio'
+                    value={ nombreEstadio }
+                    onChange={ handleInputChange }
+                    />
+                <label htmlFor="estadio">Nombre del estadio de fútbol</label>
+            </div>
+            <div className='form-floating mb-3'>
+                <input 
+                    className='form-control'
+                    type="text"
+                    placeholder='Nombre del Director técnico'
+                    id='tecnico'
+                    name='nombreTecnico'
+                    value={ nombreTecnico }
+                    onChange={ handleInputChange }
+                    />
+                <label htmlFor="tecnico">Nombre del Director técnico</label>
+            </div>
+            <div className='form-floating mb-3'>
+                <input 
+                    className='form-control'
+                    type="text"
+                    placeholder='Nombre del capitán'
+                    id='capitan'
+                    name='nombreCapitan'
+                    value={ nombreCapitan }
+                    onChange={ handleInputChange }
+                    />
+                <label htmlFor="capitan">Nombre del capitán</label>
+            </div>
+            <div className='form-floating mb-3'>
+                <input
+                    className='form-control'
+                    type="number"
+                    placeholder='Numero de títulos obtenidos'
+                    id='titulos'
+                    name='canTitulos'
+                    value={ canTitulos }
+                    onChange={ handleInputChange }
+                    />
+                <label htmlFor="titulos">Numero de títulos obtenidos</label>
+            </div>
+            
+            <select className='form-select mb-3' name='liga' value={ liga } onInput={ handleInputChange } >
+                <option value="">--Selecciona--</option>
                 <option value="laLiga">LaLiga</option>
                 <option value="ligue-1">Ligue 1</option>
                 <option value="bundesliga">Bundesliga</option>
@@ -51,16 +77,17 @@ export const Formulario = ({ initialForm }) => {
                 <option value="serie-a">Serie A</option>
             </select>
             <input 
+                className='form-control mb-3'
                 type="date"
                 name='fechaFundacion'
                 value={ fechaFundacion }
                 onChange={ handleInputChange }
                  />
-            <div>
-                <button type='submit'>
+            <div className='d-flex justify-content-evenly gap-3 align-items-center'>
+                <button className='btn btn-outline-primary' type='submit'>
                     Agregar
                 </button>
-                <button>
+                <button className='btn btn-outline-danger'>
                     Limpiar
                 </button>
             </div>
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1qbccHEyAQcqaPVQNiUKhHyCev_uygWc",
+  authDomain: "database-university.firebaseapp.com",
+  projectId: "database-university",
+  storageBucket: "database-university.appspot.com",
+  messagingSenderId: "183910226556",
+  appId: "1:183910226556:web:1157e030599363fc20c7f9"
+};
+
+// Initialize Firebase
+const FirebaseApp = initializeApp(firebaseConfig);
+const FirebaseDB = getFirestore( FirebaseApp )
